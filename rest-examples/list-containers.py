7a783ed6696ccb4600b7c5854e274494456afcb9
Emilio <e.garciacaleramolina@um.es>
2022-10-11 11:21:24 +0200

    doc en swagger + examples

--- rest-examples/list-containers.py
+++ rest-examples/list-containers.py
@@ -1,10 +1,13 @@
 #!python
 
+# curl -u 'admin:admin' -H "Accept: application/json" -X GET "http://localhost:8080/kie-server/services/rest/server/containers"
+
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-# curl -u 'admin:admin' -H "Accept: application/json" -X GET "http://localhost:8080/kie-server/services/rest/server/containers"
+host = "http://localhost:8180"
+#host = "http://192.168.7.3:8080"
 
 def send_request():
 
@@ -13,7 +16,7 @@ def send_request():
         "Accept" : "application/json"
     }
 
-    response = requests.get("http://localhost:8180/kie-server/services/rest/server/containers", auth=auth, headers = headers)
+    response = requests.get(host + "/kie-server/services/rest/server/containers", auth=auth, headers = headers)
 
     print(json.dumps(response.json(), indent=4))
 
